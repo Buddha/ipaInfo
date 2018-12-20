d1c52e4f25c67a58c4e7f3a99706c9791240120b
shenfengwin <shenfengwin@aliyun.com>
2018-12-20 16:10:30 +0800

    做了JS判定，是否是Safari浏览器

--- index.js
+++ index.js
@@ -18,3 +18,24 @@ function isWeiXin() {
         return false;
     }
 }
+
+window.onload = function () {
+    if (isSafari()) {
+        var shade = document.getElementById('Safarijpg');
+        shade.style.display = 'block';
+
+        // var nodis = document.getElementById('XiaZai');
+        // nodis.style.display = 'none';
+
+        // document.getElementById("BiaoTi").setAttribute("class","BiaoTi BiaoTi1");
+    }
+}
+
+function isSafari() {
+    var ua = window.navigator.userAgent.toLowerCase();
+    if (ua.match(/Safari/i) == 'Safari') {
+        return true;
+    } else {
+        return false;
+    }
+}
